bb7adaf64c44e3b92c4222248496af66943e2f02
Cleber F. Carvalho <cleberfc23@gmail.com>
2026-02-21 11:41:38 -0300

    test genai model, update requirements,

--- app/test_model.py
+++ app/test_model.py
@@ -1,6 +1,20 @@
 import os
 from dotenv import load_dotenv
+from google import genai
 
 load_dotenv()
 
-print("Environment loaded successfully!")
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+if not gemini_api_key:
+    raise ValueError("GEMINI_API_KEY not found")
+
+client = genai.Client(api_key=gemini_api_key)
+
+query = "Explain briefly what is Diabetes"
+response = client.models.generate_content(
+    model=os.getenv("MODEL_GEMINI_FLASH"),
+    contents=query
+)
+print(f"\nQuestion: {query}")
+print(f"GenAI: {response.text}\n")
